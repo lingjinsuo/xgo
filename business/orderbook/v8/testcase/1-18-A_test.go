81672fde4fa209bab604f51be56ddb4ad474f918
lingjinsuo <pnljs@163.com>
2026-07-12 16:37:01 +0800

    1-18-A用例

--- business/orderbook/v8/testcase/1-18-A_test.go
+++ business/orderbook/v8/testcase/1-18-A_test.go
@@ -19,3 +19,33 @@ func Test_1_18_2_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
 	RunOBTestCase(3205, "test_files/1-18-2-A.txt")
 }
+
+func Test_1_18_3_A(t *testing.T) {
+	// 传递相对于 testcase 目录的路径
+	RunOBTestCase(2403, "test_files/1-18-3-A.txt")
+}
+
+func Test_1_18_4_A(t *testing.T) {
+	// 传递相对于 testcase 目录的路径
+	RunOBTestCase(2404, "test_files/1-18-4-A.txt")
+}
+
+func Test_1_18_5_A(t *testing.T) {
+	// 传递相对于 testcase 目录的路径
+	RunOBTestCase(2404, "test_files/1-18-5-A.txt")
+}
+
+func Test_1_18_6_A(t *testing.T) {
+	// 传递相对于 testcase 目录的路径
+	RunOBTestCase(3414, "test_files/1-18-6-A.txt")
+}
+
+func Test_1_18_7_A(t *testing.T) {
+	// 传递相对于 testcase 目录的路径
+	RunOBTestCase(2921, "test_files/1-18-7-A.txt")
+}
+
+func Test_1_18_8_A(t *testing.T) {
+	// 传递相对于 testcase 目录的路径
+	RunOBTestCase(5201, "test_files/1-18-8-A.txt")
+}
--- business/orderbook/v8/testcase/testhelp.go
+++ business/orderbook/v8/testcase/testhelp.go
@@ -157,5 +157,5 @@ func RunOBTestCase(securityCode uint32, fileName string) {
 
 	// 4. 打印 OrderBook
 	orderbook.PrintOrderBookTable()
-	orderbook.PrintOrderBook(10)
+	//orderbook.PrintOrderBook(10)
 }
