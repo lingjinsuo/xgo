d94b9e1ab835fa2b3334db5f70cad7fc64f8a935
lingjinsuo <pnljs@163.com>
2026-07-15 20:48:08 +0800

    用例处理

--- business/orderbook/v8/testcase/1-18-A_test.go
+++ business/orderbook/v8/testcase/1-18-A_test.go
@@ -12,40 +12,40 @@ func TestName(t *testing.T) {
 // cat stock_market.log | grep "2421-" |grep -a "】," | grep -av "【TradeHandler】"
 func Test_1_18_1_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(2421, "test_files/1-18-1-A.txt")
+	RunOBTestCase(2421, "test_files/1-18-A/1-18-1-A.txt")
 }
 
 func Test_1_18_2_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(3205, "test_files/1-18-2-A.txt")
+	RunOBTestCase(3205, "test_files/1-18-A/1-18-2-A.txt")
 }
 
 func Test_1_18_3_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(2403, "test_files/1-18-3-A.txt")
+	RunOBTestCase(2403, "test_files/1-18-A/1-18-3-A.txt")
 }
 
 func Test_1_18_4_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(2404, "test_files/1-18-4-A.txt")
+	RunOBTestCase(2404, "test_files/1-18-A/1-18-4-A.txt")
 }
 
 func Test_1_18_5_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(2404, "test_files/1-18-5-A.txt")
+	RunOBTestCase(2405, "test_files/1-18-A/1-18-5-A.txt")
 }
 
 func Test_1_18_6_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(3414, "test_files/1-18-6-A.txt")
+	RunOBTestCase(3414, "test_files/1-18-A/1-18-6-A.txt")
 }
 
 func Test_1_18_7_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(2921, "test_files/1-18-7-A.txt")
+	RunOBTestCase(2921, "test_files/1-18-A/1-18-7-A.txt")
 }
 
 func Test_1_18_8_A(t *testing.T) {
 	// 传递相对于 testcase 目录的路径
-	RunOBTestCase(5201, "test_files/1-18-8-A.txt")
+	RunOBTestCase(5201, "test_files/1-18-A/1-18-8-A.txt")
 }
